f659718e06f8613af3ca090a0237b3261c85c7a0
luischamp03 <lchampicas@educacion.navarra.es>
2024-12-20 21:54:18 +0100

    Finished Celda class

--- src/main/java/es/masanz/ut5/buscaminas/model/Celda.java
+++ src/main/java/es/masanz/ut5/buscaminas/model/Celda.java
@@ -2,7 +2,37 @@ package es.masanz.ut5.buscaminas.model;
 
 public class Celda {
 
+    private int numero;
+    private boolean relevada;
+    private boolean bloqueada;
+
+    public Celda(int numero) {
+        this.numero = numero;
+        this.relevada = false;
+        this.bloqueada = false;
+    }
+
     public int getNumero() {
-        return -1;
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public boolean isRelevada() {
+        return this.relevada;
+    }
+
+    public void setRelevada(boolean relevada) {
+        this.relevada = relevada;
+    }
+
+    public boolean isBloqueada() {
+        return this.bloqueada;
+    }
+
+    public void setBloqueada(boolean bloqueada) {
+        this.bloqueada = bloqueada;
     }
 }
