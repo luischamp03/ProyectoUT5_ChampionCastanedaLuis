31820a27bc486bbada944cb03a320830a7d3fc26
luischamp03 <lchampicas@educacion.navarra.es>
2025-01-05 20:03:59 +0100

    Implement name register via console

--- src/main/java/es/masanz/ut5/buscaminas/app/Main.java
+++ src/main/java/es/masanz/ut5/buscaminas/app/Main.java
@@ -3,67 +3,30 @@ package es.masanz.ut5.buscaminas.app;
 import es.masanz.ut5.buscaminas.model.Buscaminas;
 import es.masanz.ut5.buscaminas.model.Dashboard;
 import es.masanz.ut5.buscaminas.model.Nivel;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import es.masanz.ut5.buscaminas.util.Configuracion;
 
-import javax.sound.midi.SysexMessage;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
+        Dashboard dashboard = new Dashboard();
 
-        System.out.println("=== BUSCAMINAS ===");
-        System.out.println("1. FACIL");
-        System.out.println("2. MEDIO");
-        System.out.println("3. DIFICIL");
-        System.out.println("4. Ver puntuaciones");
-        System.out.print("Elige una opcion: ");
-        int opcion = sc.nextInt();
+        while (true) {
+            limpiarPantalla();
 
+            System.out.println("======= BUSCAMINAS =====");
+            System.out.println("1. FACIL");
+            System.out.println("2. MEDIO");
+            System.out.println("3. DIFICIL");
+            System.out.println("4. Ver puntuaciones");
+            System.out.println("5. Salir");
+            System.out.print("Elige una opcion: ");
+            int opcion = sc.nextInt();
 
-        switch (opcion) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                Buscaminas buscaminas = new Buscaminas(Nivel.FACIL.getFilas(), Nivel.FACIL.getColumnas(), Nivel.FACIL.getBombas());
-                buscaminas.generarTablero();
-                String[][] tablero = buscaminas.obtenerTablero();
+            long tiempo = 0;
 
-                for (int i = 0; i < tablero.length; i++) {
-                    for (int j = 0; j < tablero[i].length; j++) {
-                        System.out.print("[ " + tablero[i][j] + " ]");
-                    }
-                    System.out.println();
-                }
-
-                System.out.println("Ingresa la fila de la celda (1-10): ");
-                int fila = sc.nextInt();
-                System.out.println("Ingresa la columna de la celda (1-10): ");
-                int columna = sc.nextInt();
-
-                boolean estaBloqueada = buscaminas.estaBloqueada(fila, columna);
-                if (!estaBloqueada) {
-                    buscaminas.actualizarReveladoCelda(fila, columna);
-                    if (buscaminas.getTablero()[fila][columna].getNumero() == -1) {
-                        System.out.println("Has perdido");
-                    }
-                }
-
-                tablero = buscaminas.obtenerTablero();
-
-                for (int i = 0; i < tablero.length; i++) {
-                    for (int j = 0; j < tablero[i].length; j++) {
-                        System.out.print("[ " + tablero[i][j] + " ]");
-                    }
-                    System.out.println();
-                }
-                
-                break;
-            case 4:
-                Dashboard dashboard = new Dashboard();
+            if (opcion == 4) {
                 String[][] puntuaciones = dashboard.obtenerContenido();
 
                 for (int i = 0; i < puntuaciones.length; i++) {
@@ -72,10 +35,140 @@ public class Main {
                     }
                     System.out.println();
                 }
+
+                System.out.print("Ingrese cualquier tecla para volver al menú principal...");
+                sc.nextLine();
+                sc.nextLine();
+                continue;
+            }
+
+            if (opcion == 5) {
                 break;
-            default:
-                System.out.println("Opcion incorrecta");
-                break;
+            }
+
+            Nivel dificultad = elegirDificultad(opcion);
+
+            if (dificultad == null) {
+                System.out.println("Opción incorrecta");
+                continue;
+            }
+
+            Buscaminas buscaminas = new Buscaminas(dificultad.getFilas(), dificultad.getColumnas(), dificultad.getBombas());
+            buscaminas.generarTablero();
+            String[][] tablero = buscaminas.obtenerTablero();
+
+            long tiempoInicio = System.currentTimeMillis();
+
+            while (true) {
+                limpiarPantalla();
+                mostrarTablero(tablero);
+
+                int fila = -1;
+
+                while (fila < 0 || fila > dificultad.getFilas() - 1) {
+                    System.out.print("Ingresa la fila de la celda (1-" + dificultad.getFilas() + "): ");
+                    fila = sc.nextInt() - 1;
+                }
+
+                int columna = -1;
+
+                while (columna < 0 || columna > dificultad.getColumnas() - 1) {
+                    System.out.print("Ingresa la columna de la celda (1-" + dificultad.getColumnas() + "): ");
+                    columna = sc.nextInt() - 1;
+                }
+
+                boolean estaBloqueada = buscaminas.estaBloqueada(fila, columna);
+                if (!estaBloqueada) {
+                    buscaminas.actualizarReveladoCelda(fila, columna);
+                    tablero = buscaminas.obtenerTablero();
+                    if (buscaminas.getTablero()[fila][columna].getNumero() == -1) {
+                        mostrarTablero(tablero);
+                        System.out.println("Has perdido");
+                        Thread.sleep(2000);
+                        break;
+                    }
+                }
+
+                if (buscaminas.estaResuelto()) {
+                    tablero = buscaminas.obtenerTablero();
+                    mostrarTablero(tablero);
+
+                    long tiempoFinal = System.currentTimeMillis();
+                    tiempo = (tiempoFinal - tiempoInicio) / 1000;
+
+                    boolean registroValido = false;
+                    while (!registroValido) {
+                        System.out.println("Dificultad: " + dificultad);
+                        System.out.println("Tiempo: " + tiempo + " seg");
+                        System.out.print("Ingresa tu nombre: ");
+                        sc.nextLine();
+                        String nombre = sc.nextLine();
+
+                        String[] registro = {dificultad.toString(), nombre, tiempo + ""};
+                        registroValido = dashboard.nuevoRegistro(registro);
+
+                        if (!registroValido) {
+                            System.out.println("No has ingresado un nombre valido.");
+                        }
+                    }
+
+                    break;
+                }
+            }
         }
     }
+
+    private static void limpiarPantalla() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
+
+    private static Nivel elegirDificultad(int opcion) {
+        switch (opcion) {
+            case 1:
+                return Nivel.FACIL;
+            case 2:
+                return Nivel.MEDIO;
+            case 3:
+                return Nivel.DIFICIL;
+            default:
+                return null;
+        }
+    }
+
+    private static void mostrarTablero(String[][] tablero) {
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                System.out.print("[ " + tablero[i][j] + " ]");
+            }
+            System.out.println();
+        }
+    }
+
+    private static String textoColor(String texto, String color) {
+        color = color.toUpperCase();
+        String colorElegido;
+
+        switch (color) {
+            case "AZUL":
+                colorElegido = Configuracion.AZUL_ANSI;
+                break;
+            case "ROJO":
+                colorElegido = Configuracion.ROJO_ANSI;
+                break;
+            case "VERDE":
+                colorElegido = Configuracion.VERDE_ANSI;
+                break;
+            case "MORADO":
+                colorElegido = Configuracion.MORADO_ANSI;
+                break;
+            default:
+                colorElegido = Configuracion.RESET_ANSI;
+        }
+
+        return colorElegido + texto + Configuracion.RESET_ANSI;
+    }
 }
+
+
